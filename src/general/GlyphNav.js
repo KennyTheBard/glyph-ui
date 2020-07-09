cc7c5ce5e3758e29ce82db2da108363d586c4754
Țeculescu Octavian-Alexandru <octav_teculescu@yahoo.com>
2020-07-09 16:00:22 +0300

    Fixed navbar clicking action

--- src/general/GlyphNav.js
+++ src/general/GlyphNav.js
@@ -12,12 +12,12 @@ function GlyphNav(props) {
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto">
-                    <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="#link">Link</Nav.Link>
+                    <Nav.Link>Home</Nav.Link>
+                    <Nav.Link>Link</Nav.Link>
                     <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                        <NavDropdown.Item>Action</NavDropdown.Item>
                         <NavDropdown.Divider />
-                        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                        <NavDropdown.Item>Separated link</NavDropdown.Item>
                     </NavDropdown>
                 </Nav>
             </Navbar.Collapse>
