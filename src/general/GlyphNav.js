8cff94ae30c4bc624b39ecce71269a3d8df75192
Țeculescu Octavian-Alexandru <octav_teculescu@yahoo.com>
2020-07-18 15:09:54 +0300

    Merged home and brand from nav bar

--- src/general/GlyphNav.js
+++ src/general/GlyphNav.js
@@ -4,25 +4,25 @@ import { Button, Navbar, NavDropdown, Nav } from 'react-bootstrap';
 
 import UserCorner from './UserCorner.js';
 
+import './GlyphNav.scss';
+
 function GlyphNav(props) {
     let history = useHistory();
     const user = props.user;
 
 	return (
 		<Navbar className="navbar-dark" bg="dark" expand="lg">
-            <Navbar.Brand>Glypher.io</Navbar.Brand>
+            <Navbar.Brand className="clickable" onClick={() => history.push("/")}>Glypher.io</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto">
-                    <Nav.Link onClick={() => history.push("/")}>Home</Nav.Link>
                     <Nav.Link onClick={() => history.push("/library")}>Library</Nav.Link>
                     <Nav.Link onClick={() => history.push("/workbench")}>Workbench</Nav.Link>
+                    <Nav.Link>Buy tokens</Nav.Link>
                     <NavDropdown title="More" id="basic-nav-dropdown">
                         <NavDropdown.Item>About</NavDropdown.Item>
                         <NavDropdown.Item>FAQ</NavDropdown.Item>
                         <NavDropdown.Item>Contact us</NavDropdown.Item>
-                        <NavDropdown.Divider />
-                        <NavDropdown.Item>Buy tokens</NavDropdown.Item>
                     </NavDropdown>
                 </Nav>
             </Navbar.Collapse>
